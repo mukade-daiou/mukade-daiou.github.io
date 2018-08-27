38eb1f42a8ef17060b42acbab5ca880805ad6b9f
mukadenodaiou <31959451+mukade-daiou@users.noreply.github.com>
2018-08-27 22:48:17 +0900

    Update

--- JS/make_table.js
+++ JS/make_table.js
@@ -1,11 +1,18 @@
-/*$(document).ready(function(){
-    $.getJSON("test.json",function(data){
-    for(var i in data){
-        alert("name"+data[i].name);
-        $("#list").append("<li>"+data[i].name+":"+data[i].solved+"</li>");
-    }
-});
-});*/
-$.getJSON("test.json",function(data){
-    alert(data.name);
+
+function make_table(name,json_link,tags){
+    $.getJSON(json_link,function(data){
+        for(var i in data){
+            var str="";
+            str+="<tr>";
+            for(var tag in tags){
+                str+="<td>"+data[i][tags[tag]]+"</td>";
+            }
+            str+="</tr>";
+            $("#"+name).append(str);
+        }
+    })
+}
+
+$(document).ready(function(){
+    make_table("table","https://mukade-daiou.github.io/JS/test.json",["name","solved"]);
 })
